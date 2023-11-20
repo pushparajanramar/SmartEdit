206890de831437057b1e3f03b14d51a3bd2c2427
pushparajanramar <pushparajan.ramar@capgemini.com>
2023-11-20 15:17:40 -0600

    Updated code to read from URL

--- src/pages/api/smartedit/[fileSlug].tsx
+++ src/pages/api/smartedit/[fileSlug].tsx
@@ -6,20 +6,46 @@ import path from 'path';
 import { getFullWidthTransform, getPageTitleTransformed, getProductCarasolTransform, getSectionHeadingTransform, isFullWidthBanner, isProductCarosel } from '@/utils/helper';
 import _ from 'lodash'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
     const {
         query: { fileSlug },
     } = req;
-    const sourceFilePath = path.join(process.cwd(), 'src', 'source', `${fileSlug}.json`);
     try {
-        const sourceData = JSON.parse(fs.readFileSync(sourceFilePath, 'utf-8'));
+        const isReadFromUrl = process.env.READ_FROM_URL === 'true';
+        const sourceData = isReadFromUrl ? await getJSONDataFromUrl(fileSlug as string) : await getJSONDataFromSource(fileSlug as string);
         const outputData = handleTransformData(sourceData)
         res.status(200).json(outputData);
-    } catch (error) {
+    } catch (error: { message: string } | any) {
         console.log(error, "ERROR")
-        res.status(500).json({ error: `Can't find file with name ${fileSlug}.json` });
+        res.status(500).json({ error: error.message });
     }
 };
+
+const getJSONDataFromUrl = (fileSlug: string): Record<string, any> => {
+    return new Promise((resolve, reject) => {
+        fetch(`${process.env.FILE_URL_PREFIX}${fileSlug}.json`).then((response) => {
+            if (!response.ok) {
+                reject({ message: `Can't find or read file with name ${fileSlug}.json from url` });
+            }
+            resolve(response.json())
+        })
+    })
+
+}
+
+const getJSONDataFromSource = (fileSlug: string): Record<string, any> => {
+    return new Promise((resolve, reject) => {
+        try {
+            const sourceFilePath = path.join(process.cwd(), 'src', 'source', `${fileSlug}.json`);
+            const sourceData = JSON.parse(fs.readFileSync(sourceFilePath, 'utf-8'));
+            resolve(sourceData);
+        } catch (e) {
+            reject({ message: `Can't find or read file with name ${fileSlug}.json from source` });
+
+        }
+    })
+
+}
 
 const handleTransformData = (sourceData: Record<string, any>) => {
     let modules: any = []
@@ -41,9 +67,7 @@ const handleTransformData = (sourceData: Record<string, any>) => {
                 eachOutputFromInput.push(productCaroselData)
             }
         })
-        // if (eachOutputFromInput?.length > 1) {
-            modules  = [...modules , ...eachOutputFromInput]
-        // }
+        modules = [...modules, ...eachOutputFromInput]
     })
     const outputData = getPageTitleTransformed(sourceData, modules);
     return outputData;
